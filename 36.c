21600c3b4e71464668b7d9b55ad43cfb17538000
nitesh8649 <niteshninama04849@gmail.com>
2024-09-18 08:55:23 +0530

    36.c

---
+++ 36.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int N, i;
+    
+    printf("Enter the value of N: ");
+    scanf("%d", &N);
+    
+    for (i = 1; i <= N; i += 2) {
+        printf("%d ", i);
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
